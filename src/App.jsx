39216dfa01c9143bd94b43451a434ebadbda61e7
mehtabahmad856 <mehtabahmad856@gmail.com>
2021-12-24 20:13:11 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import logo from './logo.svg'
+import React from 'react';
+import { Provider } from 'react-redux';
+import Counter from './components/counter';
+import store from './store/store';
+import './App.css';
+
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <Provider store={store}>
+      <Counter />
+    </Provider>
+    
+    
+    </div>
+  )
+}
+
+export default App
